8316ac0b25a628475a8b41f8ae174112c22c65ca
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-28 19:42:34 +0300

    Add files via upload

---
+++ Vue_Project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button @click="addLike">like</button>
+        <button @click="addDislike">dislike</button>
+    </div>Количество лайков: {{ likes }}<div>
+    </div>Количество дислайков: {{ dislikes }}<div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            likes: 0,
+            dislikes: 5,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1
+        },
+        addDislike() {
+            this.dislikes -=1
+        }
+    }
+}
+</script>
