ae74a1211807481f2f5f60c81b3b0c9718d970b2
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-30 21:06:34 +0300

    Add files via upload

--- Vue_Project/src/App.vue
+++ Vue_Project/src/App.vue
@@ -1,35 +1,77 @@
 <template>
     <div class="app">
-        <post-form
-        @create="createPost"
+        <h1>Страница с постами</h1>
+        <div class="app__btns">
+          <my-button 
+          @click="showDialog">
+          Создать пост
+        </my-button>  
+        <my-select 
+        v-model="selectedSort"
+        :options="sortOptions"
         />
+        </div>
+        
+        <my-dialog v-model:show="dialogVisivle">
+          <post-form
+        @create="createPost"
+        />  
+        </my-dialog>
         <post-list 
         :posts="posts"
-        
+        @remove="removePost"
+        v-if="!isPostsLoading"
         />
+        <div v-else>Идет загрузка</div>
     </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm"
 import PostList from "@/components/PostList"
+import MyButton from "./components/Ul/MyButton.vue";
+import axios from "axios"
+import MySelect from "@/components/Ul/MySelect"
 export default {
     components: {
-        PostList, PostForm
+        PostList, PostForm, MyButton, MySelect
     },
     data() {
         return{
-            posts: [
-                {id:1, title: "Js", body: "Описание поста"},
-                {id:2, title: "Js 2", body: "Описание поста 2"},
-                {id:3, title: "Js 3", body: "Описание поста 3"}
-            ],
+            posts: [],
+            dialogVisivle: false,
+            isPostsLoading: false,
+            selectedSort: "",
+            sortOptions: [
+                {value: "title", name: "По названию"},
+                {value: "body", name: "По содержимому"}
+            ]
         }
     },
     methods: {
         createPost(post) {
             this.posts.push(post)
-        }
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(p=>p.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisivle= true
+        },
+        async fetchPosts(){
+            try{
+                this.isPostsLoading = true
+                const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+                this.posts = response.data
+        }catch(e) {
+            alert("Error")
+             } finally{
+                this.isPostsLoading = false
+             }
+          }
+      },
+        mounted() {
+            this.fetchPosts()
     }
 }
 </script>
@@ -42,6 +84,11 @@ export default {
 }
 .app{
     padding: 20px;
+}
+.app__btns{
+    display: flex;
+    justify-content: space-between;
+    margin: 15px 0;
 }
 
 
