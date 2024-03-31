cc64fa2cbdb8e0576285de55dc952cb07998b45f
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-03-31 20:18:38 +0300

    постраничный вывод

--- Vue_Project/src/App.vue
+++ Vue_Project/src/App.vue
@@ -28,6 +28,19 @@
         v-if="!isPostsLoading"
         />
         <div v-else>Идет загрузка</div>
+        <div class="page__wrapper">
+        <div 
+        v-for="pageNumber in totalPages" 
+        :key="pageNumber"
+        class="page"
+        :class="{
+            'current-page': page === pageNumber
+        }"
+        @click="changePage(pageNumber)"
+        >
+        {{ pageNumber }}
+    </div>
+    </div>
     </div>
 </template>
 
@@ -49,6 +62,9 @@ export default {
             isPostsLoading: false,
             selectedSort: "",
             searchQuery: "",
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: "title", name: "По названию"},
                 {value: "body", name: "По содержимому"}
@@ -65,10 +81,19 @@ export default {
         showDialog() {
             this.dialogVisivle= true
         },
+        changePage(pageNumber) {
+            this.page=pageNumber
+        },
         async fetchPosts(){
             try{
                 this.isPostsLoading = true
-                const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+                const response = await axios.get("https://jsonplaceholder.typicode.com/posts",{
+                    params: {
+                        _page: this.page,
+                        _limit: this.limit
+                    }
+                })
+                this.totalPages= Math.ceil(response.headers["x-total-count"] / this.limit)
                 this.posts = response.data
         }catch(e) {
             alert("Error")
@@ -89,7 +114,9 @@ export default {
         }
     },
     watch:{
-        
+        page() {
+            this.fetchPosts()
+        }
     }
 }
 </script>
@@ -108,6 +135,15 @@ export default {
     justify-content: space-between;
     margin: 15px 0;
 }
-
-
+.page__wrapper{
+    display: flex;
+    margin-top: 15px;
+}
+.page{
+    border: 1px solid black;
+    padding: 10px;
+}
+.current-page{
+    border: 2px solid green;
+}
 </style>
