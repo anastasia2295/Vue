f46bd54e8f6a6b76894630cf47773aa14f87897e
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2024-04-01 17:58:02 +0300

    Add files via upload

--- Vue_Project/src/App.vue
+++ Vue_Project/src/App.vue
@@ -1,127 +1,21 @@
 <template>
+<Navbar></Navbar>
     <div class="app">
-        <h1>Страница с постами</h1>
-        <my-input
-          v-model="searchQuery"
-          placeholder="Поиск..."
-        />
         
-        <div class="app__btns">
-          <my-button 
-          @click="showDialog">
-          Создать пост
-        </my-button>  
-        <my-select 
-        v-model="selectedSort"
-        :options="sortOptions"
-        />
-        </div>
-        
-        <my-dialog v-model:show="dialogVisivle">
-          <post-form
-        @create="createPost"
-        />  
-        </my-dialog>
-        <post-list 
-        :posts="sortedAndSearchedPosts"
-        @remove="removePost"
-        v-if="!isPostsLoading"
-        />
-        <div v-else>Идет загрузка</div>
-        <div class="page__wrapper">
-        <div 
-        v-for="pageNumber in totalPages" 
-        :key="pageNumber"
-        class="page"
-        :class="{
-            'current-page': page === pageNumber
-        }"
-        @click="changePage(pageNumber)"
-        >
-        {{ pageNumber }}
-    </div>
-    </div>
+       <router-view>
+
+       </router-view>
     </div>
 </template>
-
 <script>
-import PostForm from "@/components/PostForm"
-import PostList from "@/components/PostList"
-import MyButton from "./components/Ul/MyButton.vue";
-import axios from "axios"
-import MySelect from "@/components/Ul/MySelect"
-import MyInput from "@/components/Ul/MyInput"
-export default {
-    components: {
-        PostList, PostForm, MyButton, MySelect, MyInput
-    },
-    data() {
-        return{
-            posts: [],
-            dialogVisivle: false,
-            isPostsLoading: false,
-            selectedSort: "",
-            searchQuery: "",
-            page: 1,
-            limit: 10,
-            totalPages: 0,
-            sortOptions: [
-                {value: "title", name: "По названию"},
-                {value: "body", name: "По содержимому"}
-            ]
-        }
-    },
-    methods: {
-        createPost(post) {
-            this.posts.push(post)
-        },
-        removePost(post) {
-            this.posts = this.posts.filter(p=>p.id !== post.id)
-        },
-        showDialog() {
-            this.dialogVisivle= true
-        },
-        changePage(pageNumber) {
-            this.page=pageNumber
-        },
-        async fetchPosts(){
-            try{
-                this.isPostsLoading = true
-                const response = await axios.get("https://jsonplaceholder.typicode.com/posts",{
-                    params: {
-                        _page: this.page,
-                        _limit: this.limit
-                    }
-                })
-                this.totalPages= Math.ceil(response.headers["x-total-count"] / this.limit)
-                this.posts = response.data
-        }catch(e) {
-            alert("Error")
-             } finally{
-                this.isPostsLoading = false
-             }
-          }
-      },
-        mounted() {
-            this.fetchPosts()
-    },
-    computed: {
-        sortedPosts() {
-            return [...this.posts].sort((post1,post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-        },
-        sortedAndSearchedPosts() {
-            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-        }
-    },
-    watch:{
-        page() {
-            this.fetchPosts()
-        }
-    }
+import Navbar from "@/components/Ul/Navbar"
+export default{
+components: {
+    Navbar
+}
 }
 </script>
-
-<style scoped>
+<style>
 *{
     margin: 0;
     padding: 0;
@@ -129,21 +23,5 @@ export default {
 }
 .app{
     padding: 20px;
-}
-.app__btns{
-    display: flex;
-    justify-content: space-between;
-    margin: 15px 0;
-}
-.page__wrapper{
-    display: flex;
-    margin-top: 15px;
-}
-.page{
-    border: 1px solid black;
-    padding: 10px;
-}
-.current-page{
-    border: 2px solid green;
 }
 </style>
